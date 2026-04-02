e9ce6ac4f64884c0da7deaf0fbcaad47557a4dad
Vi0325 <viv2503h@gmail.com>
2026-04-02 23:01:23 +0200

    Más cosas añadidas✌️

--- ToyStoryFP/Assets/Scripts/CambioEscena.cs
+++ ToyStoryFP/Assets/Scripts/CambioEscena.cs
@@ -3,15 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class CambioEscena : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("Gameplay");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+         Application.Quit();
     }
 }
