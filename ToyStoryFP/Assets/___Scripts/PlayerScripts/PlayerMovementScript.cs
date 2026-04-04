dba96273be9cc6172d4b0b362f83a5bad91fd28a
elhabana <habanaparner@gmail.com>
2026-04-04 05:25:29 +0200

    Update
    Implementacion con anims de woody

--- ToyStoryFP/Assets/___Scripts/PlayerScripts/PlayerMovementScript.cs
+++ ToyStoryFP/Assets/___Scripts/PlayerScripts/PlayerMovementScript.cs
@@ -1,8 +1,6 @@
-using System;
-using NUnit.Framework;
 using UnityEngine;
 
-public class PlayerMovementScript : MonoBehaviour
+public class MovementScript : MonoBehaviour
 {
     [Header("Movement Settings")]
     public float WalkSpeed = 5f;
@@ -11,22 +9,33 @@ public class PlayerMovementScript : MonoBehaviour
     [Header("Jump Settings")]
     public float JumpForce = 5f;
 
+    [Header("Visual Model")]
+    [SerializeField] private Transform visualModelRoot;
+    [SerializeField] private Animator modelAnimator;
+
+    private static readonly int HorizontalHash = Animator.StringToHash("Horizontal");
+    private static readonly int VerticalHash = Animator.StringToHash("Vertical");
+    private static readonly int JumpHash = Animator.StringToHash("Jump");
+
     private Vector2 input;
     private Rigidbody rb;
     private bool isGrounded;
 
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        ResolveVisualReferences();
     }
 
     void Update()
     {
         input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        UpdateMovementAnimation();
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            TriggerJumpAnimation();
         }
     }
 
@@ -37,6 +46,13 @@ public class PlayerMovementScript : MonoBehaviour
         isGrounded = false;
     }
 
+    void OnValidate()
+    {
+        if (visualModelRoot == null)
+        {
+            visualModelRoot = transform.Find("PlayerModelo");
+        }
+    }
 
     Vector3 CalculateMovement()
     {
@@ -54,5 +70,49 @@ public class PlayerMovementScript : MonoBehaviour
     void OnCollisionStay(Collision collision)
     {
         isGrounded = true;
+    }
+
+    private void ResolveVisualReferences()
+    {
+        if (visualModelRoot == null)
+        {
+            visualModelRoot = transform.Find("PlayerModelo");
+        }
+
+        if (modelAnimator == null && visualModelRoot != null)
+        {
+            modelAnimator = visualModelRoot.GetComponentInChildren<Animator>(true);
+        }
+    }
+
+    private void UpdateMovementAnimation()
+    {
+        if (modelAnimator == null)
+        {
+            ResolveVisualReferences();
+        }
+
+        if (modelAnimator == null)
+        {
+            return;
+        }
+
+        modelAnimator.SetFloat(HorizontalHash, input.x);
+        modelAnimator.SetFloat(VerticalHash, input.y);
+    }
+
+    private void TriggerJumpAnimation()
+    {
+        if (modelAnimator == null)
+        {
+            ResolveVisualReferences();
+        }
+
+        if (modelAnimator == null)
+        {
+            return;
+        }
+
+        modelAnimator.SetTrigger(JumpHash);
     }
 }
